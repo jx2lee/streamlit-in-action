eddad5b1db6cde8a98ec68124846f123e57a1ba3
jj.lee <jj.lee@coinone.com>
2023-03-07 16:58:36 +0900

    init authorization page

--- pages/authorization.py
+++ pages/authorization.py
@@ -1,15 +1,45 @@
 import streamlit as st
 
-from utils.style import HIDE_STREAMLIT_STYLE, HIDE_SIDEBAR_STYLE
+from core.slack import request_auth_slack
+from utils.style import FAVICON_IMG, default_markdown
 
 
-def view():
-    st.markdown(HIDE_STREAMLIT_STYLE, unsafe_allow_html=True)
-    st.markdown(HIDE_SIDEBAR_STYLE, unsafe_allow_html=True)
+def auth_view():
+    st.set_page_config(
+        page_title='Authorization',
+        page_icon=FAVICON_IMG,
+        initial_sidebar_state='collapsed'
+    )
+    default_markdown()
 
-    st.write("""
-    kkkkkkkkkkk
-    """)
+    st.title('Authorization')
+
+    selected_services = st.multiselect(
+        label='Service(s): ',
+        options=['BigQuery', 'Lightdash', 'DataHub']
+    )
+    if selected_services:
+        st.write('You selected', len(selected_services), 'service(s)')
+
+    username = st.text_input(label='Enter your name.')
+    if username:
+        st.success(f'username: {username}')
+
+    email = st.text_input(label='Enter your email.')
+    if email:
+        st.success(f'email: {email}')
+
+    if selected_services and username and email:
+        st.button(
+            label='submit',
+            use_container_width=True,
+            on_click=request_auth_slack(
+                services=selected_services,
+                username=username,
+                email=email
+            )
+        )
 
 
-view()
+if __name__ == '__main__':
+    auth_view()
